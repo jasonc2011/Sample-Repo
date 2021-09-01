eca8fb2083a69eadba1dcb92d0c40ab1189d8b93
jasonc2011 <jasonclights@gmail.com>
2021-09-01 14:41:35 -0400

    Initial commit

---
+++ script.py
@@ -0,0 +1,14 @@
+import sys
+import requests
+import os
+import math
+
+#print(sys.version)
+#print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+r = requests.get('https://www.washingtonpost.com')
+print(r.status_code)
